0bcf44aa2b8df4bc000dba5aa95a506aa658ba40
Felipe Freitas <31215284+felipefreitasdeoliveira@users.noreply.github.com>
2020-05-19 21:55:59 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo snap install code --classic
+sudo snap install eclipse --classic
+sudo snap install telegram-desktop
+sudo snap install slack --classic
+sudo snap install vlc
+sudo snap install shotcut --classic
+sudo snap install spotify
+sudo snap install youtube-dl
+sudo snap install obs-studio
+sudo snap install inkscape
+sudo snap install sublime-text --classic
+sudo snap install postman
+sudo snap install redis-desktop-manager
+sudo snap install pycharm-educational --classic
+sudo snap install storage-explorer
+sudo snap install dynamodb-gui-client
+sudo snap install mysql-workbench-community --candidate
+
+sudo apt install git telnet -y
+
