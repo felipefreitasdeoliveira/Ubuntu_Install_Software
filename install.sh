02997044c660f650d3b43893d2837d3c3eb58a39
Felipe Freitas <31215284+felipefreitasdeoliveira@users.noreply.github.com>
2020-05-19 22:46:42 -0300

    Add Brave

--- install.sh
+++ install.sh
@@ -17,5 +17,12 @@ sudo snap install storage-explorer
 sudo snap install dynamodb-gui-client
 sudo snap install mysql-workbench-community --candidate
 
-sudo apt install git telnet -y
+sudo apt install git telnet apt-transport-https curl -y
 
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser
