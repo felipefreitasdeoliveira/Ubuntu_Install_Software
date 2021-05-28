59ed3966061a5bb4a6f69d222e41383b73ffa038
Felipe Freitas <31215284+felipefreitasdeoliveira@users.noreply.github.com>
2021-05-28 20:19:25 -0300

    # Install eksctl binary

--- Config_Ubuntu_base.sh
+++ Config_Ubuntu_base.sh
@@ -50,4 +50,9 @@ chmod +x kubectl
 sudo mv kubectl /usr/local/bin/kubectl
 kubectl version
 
+# Install eksctl binary 
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
 
