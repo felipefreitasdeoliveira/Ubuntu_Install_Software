beb2771d5dabe1c99061c6b628fd4ebd0701d1a7
Felipe Freitas <31215284+felipefreitasdeoliveira@users.noreply.github.com>
2021-05-28 20:17:39 -0300

    # Install Install kubectl binary

--- Config_Ubuntu_base.sh
+++ Config_Ubuntu_base.sh
@@ -42,3 +42,12 @@ pip3 install awsebcli --upgrade --user
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops
 sudo mv kops /usr/local/bin/kops
+
+
+# Install Install kubectl binary 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+kubectl version
+
+
